b6126f8672931a5a0ac9f020d31f3f69ed88bc91
DreamCat04 <112375774+DreamCat04@users.noreply.github.com>
2023-09-29 15:01:02 +0200

    Implement element structure

--- src/components/MessagesDiv.js
+++ src/components/MessagesDiv.js
@@ -1,10 +1,25 @@
 import '../css/MessagesDiv.css'
+
+
+
 function MessagesDiv(){
     return(
         <div className='messagesDiv'>
             <div id='messageHeader'>
                 <img src='' alt=''></img>
                 <p>Name of first contact</p>
+            </div>
+            <div id='messageBody'>
+                <ul id='messages'>
+                    <li class='message'>
+                        <img src='test'></img>
+                        <p>First message</p>
+                    </li>
+                    <li class='message'>
+                        <img src='test'></img>
+                        <p>Second message</p>
+                    </li>
+                </ul>
             </div>
         </div>
     );
