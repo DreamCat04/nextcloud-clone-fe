650c2e86563cb64c8069e100a2666501cd571fe1
DreamCat04 <112375774+DreamCat04@users.noreply.github.com>
2023-10-27 14:53:59 +0200

    Fetch messages from API instead of from useState

--- src/components/MessagesContainer.js
+++ src/components/MessagesContainer.js
@@ -1,17 +1,25 @@
 import Message from './Message'
-const [messages, setMessages] = useState([]);
-const addMessage = (text, sender) => {
-    const newMessage = { text, sender };
-    setMessages([...messages, newMessage]);
-    handleSendMessage();
-};
+import axios from 'axios';
+import MessageBox from './MessageBox';
 
 function MessagesContainer(){
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+    // Fetch messages and set the state here
+    axios.get('/api/messages').then((response) => {
+        setMessages(response.data);
+        }).catch((error) => {
+        console.error('Error fetching messages:', error);
+        });
+    }, []);
+
     return(
-        <div>
+        <div className='messagesContainer'>
             {messages.map((message, index) => (
             <Message key={index} text={message.text} sender={message.sender}/>
             ))}
+            <MessageBox></MessageBox>
         </div>
     );
 }
