11ad833a2c5e792f0f6c2e4ca1346822af635e23
DreamCat04 <112375774+DreamCat04@users.noreply.github.com>
2023-10-27 14:53:03 +0200

    Remove no longer needed useState elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
 import ContactsDiv from './components/ContactsDiv';
 import MessagesContainer from './components/Message';
-import { useState } from 'react';
 
 function App() {
-  const [messages, setMessages] = useState([]);
   return (
     <div className="App">
       <ContactsDiv></ContactsDiv>
