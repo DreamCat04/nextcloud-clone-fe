cb43906a7b854c274217cf70d24ecd802327f551
DreamCat04 <112375774+DreamCat04@users.noreply.github.com>
2023-11-02 17:29:44 +0100

    Add proper message layout

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,18 @@
 import '../css/Message.css'
 
-function Message({id, text, sender}) {
-    return (
-      <div className={`message_${sender}`}>
-        <p>{text}</p>
+function Message({ text, sender, avatar, date, isMyMessage }) {
+  return (
+    <div className={`message ${isMyMessage ? 'my-message' : 'opponent-message'}`}>
+      <div className="message-avatar">
+        <img src={avatar} alt={`${sender}'s avatar`} />
       </div>
-    );
+      <div className="message-content">
+        <div className="message-username">{sender}</div>
+        <div className="message-text">{text}</div>
+        <div className="message-date">{date}</div>
+      </div>
+    </div>
+  );
 }
 
 export default Message;
