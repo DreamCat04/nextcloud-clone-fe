76b79f19d37a87b3a304a0ca8a22788397b8c85a
DreamCat04 <112375774+DreamCat04@users.noreply.github.com>
2023-11-10 14:59:18 +0100

    Move sendMessageToAPI inside of component function

--- src/components/MessageInputForm.js
+++ src/components/MessageInputForm.js
@@ -1,31 +1,30 @@
 import axios from "axios";
 import { useState } from "react";
 
-async function sendMessageToAPI(message){
-    try {
+function MessageInputForm(){
+
+  const [message, setMessage] = useState();
+
+  async function sendMessageToAPI(message){
+  try {
     const response = await axios.post('http://localhost:5000/api/messages', {
-      text: message,
-    });
+    text: message,
+  });
     console.log('Message sent successfully:', response.data);
-    // Handle success, update the UI, etc.
   } catch (error) {
     console.error('Error sending message:', error);
-    // Handle errors, show an error message, etc.
   }
 }
-
-function MessageInputForm(){
-  const [message, setMessage] = useState();
-    return (
-        <div className="messageInputForm">
-            <input
-            type="text"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            />
-            <button onClick={() => sendMessageToAPI(message)}>Send</button>
-        </div>
-    );
+  return (
+      <div className="messageInputForm">
+          <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          />
+          <button onClick={() => sendMessageToAPI(message)}>Send</button>
+      </div>
+  );
 }
 
 export default MessageInputForm;
