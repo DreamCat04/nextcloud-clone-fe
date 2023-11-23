77725842331bd95b8cc7e1a9bf584b090e23ca7a
DreamCat04 <112375774+DreamCat04@users.noreply.github.com>
2023-11-23 16:03:50 +0100

    Implement redirection from login page to main page on successful login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,32 +1,41 @@
 import React, { useState } from 'react';
 import '../css/LoginForm.css'
+import { useNavigate } from 'react-router-dom';
+
 
 function LoginForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-
+  const [errorMessage, setErrorMessage] = useState('');
+  const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     // Send the credentials to the backend for verification
     try {
-      const response = await fetch('http://localhost:5000/login', {
+      const response = await fetch('http://localhost:5000/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ username, password }),
+        body: JSON.stringify({ username, password })
       });
 
-      const data = await response.json();
-
+      if (response.ok) {
+        // If the response is successful, redirect to the main page
+        navigate('/');
+      } else {
+        // If the response is not successful, set an error message
+        setErrorMessage('Invalid credentials. Please try again.');
+      }
     } catch (error) {
       console.error('Error:', error);
     }
-  };
+  }
 
   return(
     <form className="login-form" onSubmit={handleSubmit}>
+      {errorMessage && <div className="error-message">{errorMessage}</div>}
       <div>
         <label htmlFor="username">Username</label><br/>
         <input
@@ -48,6 +57,6 @@ function LoginForm() {
       <button type="submit">Login</button>
     </form>
   );
-};
+}
 
 export default LoginForm;
